d8008052d26252252aae27f09087d7d4c9c132b6
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-08-09 04:46:16 +0600

    setup ready

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <Login></Login>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+export default {
+  name: 'PharmacyManagementApp',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  components:{
+    Login
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="login-page">
+        <div class="login-card">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+.login-page {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(235, 235, 235);
+}
+
+.login-card {
+  width: 400px;
+  min-height: 193px;
+  box-shadow: 0px 2px 9px 4px #dfdfdf;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 44px 33px;
+}
+
+.login-card input[type="email"],
+.login-card input[type="password"] {
+  width: 100%;
+}
+
+.login-card__icon {
+  max-width: 77px;
+}
+
+@keyframes showhide {
+  0% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+
+  50% {
+    opacity: 0.5;
+    transform: scale(1.2);
+  }
+
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+</style>
